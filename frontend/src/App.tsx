fa80f8351ee014a060284020d1a206e1a3d0f812
Anish Mistry <anishmistry312@gmail.com>
2025-12-15 19:27:11 -0500

    chore: initialise project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+function App() {
+  return <div className="text-3xl font-bold text-red-500">Tailwing works!</div>;
+}
+
+export default App;
